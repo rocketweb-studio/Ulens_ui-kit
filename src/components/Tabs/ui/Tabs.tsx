467be5639a50003a0528cf4769ba9e734700a6fc
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-11-16 21:40:56 +0300

    v0.9.16 Tabs refactor

--- src/components/Tabs/ui/Tabs.tsx
+++ src/components/Tabs/ui/Tabs.tsx
@@ -1,27 +1,33 @@
 import s from './Tabs.module.scss'
 import {FlexContainer} from "../../FlexContainer"
+import  { MouseEvent } from 'react'
 
 export type TabsProps = {
     tabsSettings:{ title: string,   }[]
     href?:string
-    activeTabTitle:string
+    setActiveTab:(value:string) => void
+    activeTab:string
 }
 
-export const Tabs = ({ tabsSettings,href,activeTabTitle }: TabsProps) => {
-
+export const Tabs = ({ tabsSettings,href,activeTab,setActiveTab }: TabsProps) => {
+    const onClickHandler = (e:MouseEvent<HTMLAnchorElement>): void => {
+        e.preventDefault();
+        setActiveTab(e.currentTarget.textContent || "");
+    }
   return (
       <FlexContainer wrap justify={'center'}>
         {tabsSettings.map(({ title }, index) => {
-          const isActive = activeTabTitle === title
-          return <a
+          const isActive = activeTab === title
+          return (
+              <a
+              onClick={onClickHandler}
               href={href||""}
               key={index}
               className={`${s.link} ${isActive ? s.activeLink : ''}`}
-
           >
             {title}
           </a>
-        })}
+          )})}
       </FlexContainer>
   )}
 
